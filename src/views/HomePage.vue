abde4f572c2b267a3699ad13fc6dc5dd899a5832
Maxence Machu <maxence@akarah.com>
2022-05-03 21:48:29 +0200

    add(buttons)

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,52 +1,72 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
+    <ion-content class="cs-dark-bg" :fullscreen="true">
+      <ion-fab
+        vertical="bottom"
+        class="ion-padding-bottom"
+        horizontal="center"
+        slot="fixed"
+      >
+        <ion-fab-button color="dark" @click="openLibrary()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+      <ion-fab
+        class="ion-padding-bottom ion-padding-start"
+        vertical="bottom"
+        horizontal="left"
+        slot="fixed"
+      >
+        <ion-fab-button color="dark" @click="takePhoto()">
+          <ion-icon :icon="folder"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>
-          Start with Ionic
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://ionicframework.com/docs/components"
-            >UI Components</a
-          >
-        </p>
-      </div>
+      <ion-fab
+        class="ion-padding-bottom ion-padding-end"
+        vertical="bottom"
+        horizontal="end"
+        slot="fixed"
+      >
+        <ion-fab-button color="dark" @click="switchCamera()">
+          <ion-icon :icon="sync"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
     </ion-content>
   </ion-page>
 </template>
 
+<style scoped>
+ion-content {
+  --background: #000 no-repeat center center / cover;
+}
+</style>
 <script>
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/vue";
+import { IonContent, IonPage } from "@ionic/vue";
 import { defineComponent } from "vue";
-
+import { camera, folder, sync } from "ionicons/icons";
 export default defineComponent({
   name: "HomePage",
   components: {
     IonContent,
-    IonHeader,
     IonPage,
-    IonTitle,
-    IonToolbar,
+  },
+  setup() {
+    return {
+      takePhoto() {
+        alert("take photo");
+      },
+      openLibrary() {
+        alert("open lib");
+      },
+      switchCamera() {
+        alert("switch");
+      },
+      camera,
+      folder,
+      sync,
+    };
   },
 });
 </script>
